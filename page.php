914707c2af5cedb85cf0e3a7905d3e00aa85effd
ivanuria <ivan.uria@gmail.com>
2021-02-10 13:10:23 +0100

    Hero creation, not tested.

--- page.php
+++ page.php
@@ -1,13 +1,24 @@
 <?php get_header() ?>
     <main>
+      ?>
+      <div class='hero_container'>
+        <?php
+          if (get_theme_mod('archresp_hero_page_active')) {
+        ?>
+        <div class='hero_image' style="background-image:url('<?php if(has_post_thumbnail()){the_post_thumbnail();} ?>')">
+        </div>
+        <?php
+          }
+        ?>
+        <div class='hero_title container'>
+          <h1><?php the_title() ?></h1>
+        </div>
+      </div>
       <article class='container'>
         <?php
           if(have_posts()) {
             while(have_posts()){
               the_post();
-              ?>
-              <h1><?php the_title() ?></h1>
-              <?php
               the_content();
             }
           }
